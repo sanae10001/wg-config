36167d703c8daeacb05f0ec9ab3a7eb75fc6b0a7
Yingjie Run <loveKotiya@hotmail.com>
2018-10-26 15:29:28 +0800

    add openwrt route script

---
+++ openwrt/addroute.sh
@@ -0,0 +1,30 @@
+setname="chnroute"
+routefilepath=$HOME/$setname.txt
+setfilepath=$HOME/$setname
+
+genroutefile() {
+    test -e "$routefilepath" || wget -O- 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > "$routefilepath"
+
+    ipset create $setname hash:net -exist
+    while read -r ips; do
+        ipset add $setname $ips -exist
+    done < "$routefilepath"
+
+    ipset save $setname > "$setfilepath"
+    # ipset destroy $setname
+}
+
+if [-e "$setfilepath"] 
+then
+    ipset restore < "$setfilepath"
+else
+    genroutefile
+fi
+
+iptables -t mangle -A PREROUTING -m set --match-set $setname dst -j MARK --set-mark 100
+wg set wg fwmark 1234
+
+ip route add default dev wg table 2468
+
+ip rule add not fwmark 1234 table 2468 pref 40
+ip rule add table main suppress_prefixlength 0
---
+++ openwrt/setupwg.sh
@@ -0,0 +1,16 @@
+# network.wg=interface
+# network.wg.proto='wireguard'
+# network.wg.private_key='WPmlnNsJ8E0WrQw6P6qUxHyiUS9sE2E4XHqPc/tni1s='
+# network.wg.addresses='10.0.0.2/24'
+# network.wg.mtu='1300'
+# network.wg.listen_port='51820'
+# network.wg.auto='0'
+# network.@wireguard_wg[0]=wireguard_wg
+# network.@wireguard_wg[0].public_key='m/vCfOjN/fLq8tZH53uwL/Ve2NzrzzNDcTcE+fUnjxE='
+# network.@wireguard_wg[0].endpoint_host='52.196.249.143'
+# network.@wireguard_wg[0].endpoint_port='51820'
+# network.@wireguard_wg[0].persistent_keepalive='25'
+# network.@wireguard_wg[0].allowed_ips='0.0.0.0/0'
+# network.@wireguard_wg[0].route_allowed_ips='0'
+
+# uci add network interface wg
